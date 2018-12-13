3101fbefc27d40a3a621dcdbb1f3ca990ee182c3
Yuanye <geyuanye@gmail.com>
2018-12-13 16:41:29 +0800

    Modify copywriter

--- src/components/TransactionDetail.js
+++ src/components/TransactionDetail.js
@@ -11,10 +11,10 @@ export default class Transaction extends PureComponent {
       <div className={styles.tab_content}> 
         <p>BlockNum: {tranaction.block_info.number}</p> 
         <p>BlockHash: {tranaction.block_info.current_hash}</p>
-        <p>PreviousHash: {tranaction.block_info.previous_hash}</p>
+        <p>PreviousBockHash: {tranaction.block_info.previous_hash}</p>
         <div> Txid: {tranaction.tx_id}</div>
         <div> UserHash: {tranaction.key}</div>
-        <div> HashValue: {tranaction.value}</div>
+        <div> OrderHash: {tranaction.value}</div>
         <div> Channel: {tranaction.channel_id}</div>
         <div> Status: {tranaction.validation_code_name}</div>
         <div> Timestamp : {tranaction.timestamp.seconds}</div>
